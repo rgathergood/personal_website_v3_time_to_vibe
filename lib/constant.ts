91a72db9a8e317d29212785d5d383bfa307da9f8
Rob <33901093+rgathergood@users.noreply.github.com>
2026-02-18 11:38:41 +0000

    Create constant.ts

---
+++ lib/constant.ts
@@ -0,0 +1,16 @@
+export const SITE_NAME = 'Rob Gathergood'
+export const SITE_URL = 'https://robgathergood.com'
+export const SITE_DESCRIPTION = 'Product Manager at Payscale. Musician, renewable energy enthusiast, and builder.'
+
+export const SOCIAL_LINKS = {
+  linkedin: 'https://www.linkedin.com/in/rob-gathergood/',
+  email: 'rgathergood@gmail.com',
+  github: '#',
+  twitter: '#'
+}
+
+export const CONTACT_INFO = {
+  email: 'rgathergood@gmail.com',
+  phone: '+44 (0) 7854 558 449',
+  location: 'UK'
+}
